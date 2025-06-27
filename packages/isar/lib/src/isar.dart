3292a8bc9d273077596cc0058acd8d35fd022b57
W <130329555+wcat7@users.noreply.github.com>
2025-06-27 15:11:35 +0800

    update dependencies & version

--- packages/isar/lib/src/isar.dart
+++ packages/isar/lib/src/isar.dart
@@ -21,7 +21,7 @@ abstract class Isar {
   static const int defaultMaxSizeMiB = 128;
 
   /// The current Isar version.
-  static const String version = '0.0.0-placeholder';
+  static const String version = '4.0.3';
 
   /// Use this value for the `directory` parameter to create an in-memory
   /// database.
